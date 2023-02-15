f7409f56d727359ba0726d1857eaea9f4e747850
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-02-16 01:03:19 +0300

    Insert book

--- book-store-app/src/components/AddForm.js
+++ book-store-app/src/components/AddForm.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { insertBook } from '../store/bookSlice';
 
 const Addform = () => {
+  const title = useRef(null);
+  const price = useRef(null);
+  const description = useRef(null);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const bookData = {
+      title: title.current.value,
+      price: price.current.value,
+      description: description.current.value,
+    };
+    dispatch(insertBook(bookData));
+    title.current.value = null;
+    price.current.value = null;
+    description.current.value = null;
+  };
+
   return (
-    <div className='row'>
-      <div className='col-6 offset-3 mt-3'>
+    <div className="row">
+      <div className="col-6 offset-3 mt-3">
         <h2>Insert Book</h2>
-        <form>
-          <div className='form-group'>
-            <label htmlFor='title'>Title</label>
-            <input type='text' className='form-control' id='title' required />
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              required
+              ref={title}
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='price'>Price</label>
-            <input type='number' className='form-control' id='price' required />
+          <div className="form-group">
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              className="form-control"
+              id="price"
+              required
+              ref={price}
+            />
           </div>
-          <div className='form-group'>
-            <label htmlFor='Description'>Description</label>
+          <div className="form-group">
+            <label htmlFor="Description">Description</label>
             <textarea
-              className='form-control'
-              id='Description'
-              rows='3'
+              ref={description}
+              className="form-control"
+              id="Description"
+              rows="3"
               required
             ></textarea>
           </div>
-          <button type='submit' className='btn btn-primary'>
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
--- book-store-app/src/store/bookSlice.js
+++ book-store-app/src/store/bookSlice.js
@@ -13,11 +13,31 @@ export const fetchBooks = createAsyncThunk(
   }
 );
 
+export const insertBook = createAsyncThunk(
+  'book/insertBooks',
+  async (data, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+    try {
+      const result = await fetch('http://localhost:3005/books', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      return result.json();
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
 const bookSlice = createSlice({
   name: 'book',
   initialState: { books: [], isLoading: false, error: null },
   reducers: {},
   extraReducers(builder) {
+    // Fetch Books
     builder.addCase(fetchBooks.pending, (state, action) => {
       state.isLoading = true;
     });
@@ -28,6 +48,21 @@ const bookSlice = createSlice({
     });
 
     builder.addCase(fetchBooks.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+
+    // Insert Book
+    builder.addCase(insertBook.pending, (state, action) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(insertBook.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.books.push(action.payload);
+    });
+
+    builder.addCase(insertBook.rejected, (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
     });
